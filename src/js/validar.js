0738e8b6b2f1d5c723b87c48b61742d6f8a48ffa
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-07-19 13:31:58 -0300

    validar formulario

---
+++ src/js/validar.js
@@ -0,0 +1,32 @@
+const itemsDoForm = Array.from(document.querySelectorAll('.item'));
+const botaoEnviar = document.getElementById("btn-enviar");
+
+botaoEnviar.addEventListener("click", function (event) {
+
+
+  itemsDoForm.forEach(function (itemSelecionado) {
+    let valor = itemSelecionado.querySelector('.form1');
+    let erroPreenchimento = itemSelecionado.querySelector('.texto-erro');
+
+    
+      if (valor.value !== "") {
+        valor.classList.remove("faltou-prencher");
+        valor.classList.add("preenchido");
+
+        erroPreenchimento.classList.add("tirar-erro");
+
+      } else {
+
+        valor.classList.remove("preenchido");
+        valor.classList.add("faltou-prencher");
+
+        erroPreenchimento.classList.remove("tirar-erro");
+
+        event.preventDefault();
+
+      }
+   
+  });
+});
+
+
